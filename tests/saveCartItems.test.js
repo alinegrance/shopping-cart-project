2c8d8a840addfb82747ddad0876eec0f4fe4b046
Aline Grance <grance.aline@gmail.com>
2022-08-09 19:25:54 -0300

    Req6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se ao exercutar saveCartItems com o argumento "<ol><li>Item</li></ol>" o método localStorage.setItem é chamado', () => {
+    const item = '<ol><li>Item</li></ol>'
+    saveCartItems(item);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Testa se ao exercutar saveCartItems com o argumento "<ol><li>Item</li></ol>" o método localStorage.setItem é chamado com dois parâmetros: cartItems e valor ', () => {
+    const item = '<ol><li>Item</li></ol>'
+    saveCartItems(item);
+    expect(localStorage.setItem).toHaveBeenCalledWith(cartItems, item);
+  });
 });
